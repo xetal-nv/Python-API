e95db7eaed62dfc36e5ee8c42773c0b4469dcb14
fpessolano <francesco@fralke.com>
2017-07-16 07:48:10 +0200

    Release v.4.0.0 development

--- libs/gui.py
+++ libs/gui.py
@@ -29,41 +29,19 @@ class StartGUI:
         self.ipEntry = Entry(master, width=20)
         self.ipEntry.pack(side=TOP, padx=10, pady=10)
         if os.path.isfile(SAVEDIP):
-            with (open(SAVEDIP,'r')) as savedIP:
+            with (open(SAVEDIP, 'r')) as savedIP:
                 ip = savedIP.read().strip('\n')
                 self.ipEntry.insert(0, ip)
         else:
-            self.ipEntry.insert(0,"192.168.76.1") # remove comment to set as default the standard AP address
+            self.ipEntry.insert(0, "192.168.76.1")  # remove comment to set as default the standard AP address
 
         # bind escape to terminate
         master.bind('<Escape>', quit)
 
-        Button(master, text='Connect IP', command=self.connectDeviceIP).pack(side=LEFT, padx=10, pady=5)
-        Button(master, text='Connect DNS', command=self.connectDeviceDNS).pack(side=LEFT, padx=10, pady=5)
+        Button(master, text='Connect', command=self.connectDevice).pack(side=LEFT, padx=10, pady=5)
         Button(master, text='Quit', command=master.quit).pack(side=RIGHT, padx=10, pady=5)
 
-    def connectDeviceIP(self):
-        # the module ip address is used to verify the validity of the entered IP address
-        try:
-            ipaddress.ip_address(self.ipEntry.get())
-            ip = self.ipEntry.get()
-        except:
-            self.ipEntry.configure(fg="red")
-            return
-
-        self.ipEntry.configure(fg="black")
-        self.device.connect(ip)
-
-        if self.device.isConnected():
-            self.master.destroy()
-            with (open(SAVEDIP,'w')) as savedIP:
-                savedIP.write(ip)
-            self.device.start()
-        else:
-            self.ipEntry.configure(fg="red")
-
-    def connectDeviceDNS(self):
-        # the module ip address is used to verify the validity of the entered IP address
+    def connectDevice(self):
 
         self.ipEntry.configure(fg="black")
         ip = self.ipEntry.get()
@@ -71,7 +49,7 @@ class StartGUI:
 
         if self.device.isConnected():
             self.master.destroy()
-            with (open(SAVEDIP,'w')) as savedIP:
+            with (open(SAVEDIP, 'w')) as savedIP:
                 savedIP.write(ip)
             self.device.start()
         else:
